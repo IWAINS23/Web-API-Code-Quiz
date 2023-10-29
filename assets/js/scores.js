ded66186fed82538a1a3104268efd20a23667433
abc <abc@example.com>
2023-10-29 17:46:26 +0000

    I have added a sction to the highscore.js to allow for the initials stored in the local storage to be called and sorted in a list of Highest to lowest scores

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,4 +1,18 @@
 function printHighScores (){
+let highscore = JSON.parse(localStorage.getItem("highscores") || [])
+
+highscore.sort(function(a,b){
+    return b.score - a.score;
+})
+
+highscore.forEach(function(score){
+    let li = document.createElement("li")
+    li.textContent = `${score.initals} - ${score.score}`
+
+    let ol = document.getElementById("highscores")
+    ol.appendChild(li);
+
+})
 
 }
 
